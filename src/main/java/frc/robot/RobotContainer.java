19870e5d73149c25ea241bbe3bfa3b652cbfdab8
Spencer McGrath <Samcgrath2005@gmail.com>
2024-01-31 18:19:29 -0500

    Merged autos into prototype

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -29,6 +29,7 @@ public class RobotContainer {
    public static SUB_Shooter shooter = new SUB_Shooter();
    public static SUB_Intake intake = new SUB_Intake();
    public static SUB_Pivot pivot = new SUB_Pivot();
+   public static AutoGenerator autod = new AutoGenerator(drivetrain);
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController DriverC =
