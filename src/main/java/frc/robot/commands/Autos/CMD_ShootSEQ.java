efa9056ea1e108029409112aefdda21e0420bdda
Spencer McGrath <Samcgrath2005@gmail.com>
2024-03-02 09:20:39 -0500

    SEQ shooting

--- src/main/java/frc/robot/commands/Autos/CMD_ShootSEQ.java
+++ src/main/java/frc/robot/commands/Autos/CMD_ShootSEQ.java
@@ -4,49 +4,33 @@
 
 package frc.robot.commands.Autos;
 
-import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.Constants;
-import frc.robot.subsystems.SUB_Index;
-import frc.robot.subsystems.SUB_Pivot;
-import frc.robot.subsystems.SUB_Shooter;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants.*;
+import frc.robot.subsystems.*;
 
-public class CMD_ShootSEQ extends Command {
-  /** Creates a new CMD_ShootSEQ. */
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class CMD_ShootSEQ extends SequentialCommandGroup {
+
+
   SUB_Shooter shooter;
   SUB_Index index;
   SUB_Pivot pivot;
-  
-
-  /** Creates a new CMD_Shoot. */
+  CMD_Shoot CMD_shoot;
+    /** Creates a new CMD_ShootSEQ. */
   public CMD_ShootSEQ(SUB_Shooter shooter, SUB_Index index, SUB_Pivot pivot) {
+
     this.shooter = shooter;
     this.index = index;
     this.pivot = pivot;
-    // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(shooter, index, pivot);
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+
+    addCommands(new InstantCommand(()->pivot.setPivotSetpoint(Pivot.kAmpAngleSP)), new CMD_Shoot(shooter, index));
   }
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-   
-  }
+ 
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    pivot.setPivotSetpoint(Constants.Pivot.kAmpAngleSP);
-    new CMD_Shoot(shooter, index).schedule();
-    
-  }
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
 }
