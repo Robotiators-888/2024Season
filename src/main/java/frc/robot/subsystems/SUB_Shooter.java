6cf56ca1ca4ea7d98fe4f736e753310485078254
Spencer McGrath <Samcgrath2005@gmail.com>
2024-03-08 15:41:58 -0500

    Update SUB_Shooter.java

--- src/main/java/frc/robot/subsystems/SUB_Shooter.java
+++ src/main/java/frc/robot/subsystems/SUB_Shooter.java
@@ -2,6 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 package frc.robot.subsystems;
+import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.CANSparkMax;
