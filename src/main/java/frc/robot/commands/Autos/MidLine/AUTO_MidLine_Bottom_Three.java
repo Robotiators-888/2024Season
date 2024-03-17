3af8fe353184b36ae36066382881de44b6b45051
Spencer McGrath <Samcgrath2005@gmail.com>
2024-03-17 07:26:04 -0400

    Made a source Auto, I couldn't help myself

--- src/main/java/frc/robot/commands/Autos/MidLine/AUTO_MidLine_Bottom_Three.java
+++ src/main/java/frc/robot/commands/Autos/MidLine/AUTO_MidLine_Bottom_Three.java
@@ -10,19 +10,26 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants.Pivot;
 import frc.robot.commands.Autos.AutoPaths;
 import frc.robot.utils.AutoGenerator;
+import frc.robot.utils.PathPlannerBase;
 
 /** Add your docs here. */
-public class AUTO_MidLine_Beline extends AutoPaths {
+public class AUTO_MidLine_Bottom_Three extends AutoPaths {
 
     @Override
     public Command load(AutoGenerator autos) {
-        String p1Name = "SillyAuto_Start_Top_1";
-        String p2Name = "";
+        String p1Name = "BottomStart_5GP";
+        String p2Name = "5th_to_BottomGP";
+        String p3Name = "Bottom_to_4GP";
+        String p4Name = "4th_to_BottomGP";
+        
         PathPlannerPath p1 = PathPlannerPath.fromPathFile(p1Name);
         var alliance = DriverStation.getAlliance();
-    
+        
+        // 3.07, 3.09, 147.99
+
         Pose2d startingPose = null;
         if (alliance.isPresent()) {
             if (alliance.get() == DriverStation.Alliance.Red){
@@ -30,16 +37,21 @@ public class AUTO_MidLine_Beline extends AutoPaths {
             } else {
                 startingPose = p1.getPreviewStartingHolonomicPose();
             }
-        } 
-
+        }
 
         return new SequentialCommandGroup(
+            autos.scoringSequence(Pivot.kSpeakerAngleSP-6,4000, 0.45),
             autos.resetOdometry(startingPose),
-            autos.pathIntake(p1Name)
+
+            autos.pathIntake(p1Name).withTimeout(4),
+            PathPlannerBase.followTrajectory(p2Name).withTimeout(4),
+            autos.scoringSequence(Pivot.kLowAngleSP-1, 4500, 0.5),
+
+            autos.pathIntake(p3Name).withTimeout(4),
+            PathPlannerBase.followTrajectory(p4Name).withTimeout(4),
+            autos.scoringSequence(Pivot.kLowAngleSP-1, 4500, 0.5)
 
         );
     }
-
-
     
 }
