a274af94a0fda6d763e60b2ef658f87300c8b7c2
Kevin Hwang <83924133+KevinH45@users.noreply.github.com>
2024-03-20 17:20:19 -0400

    tuned pid and index timer (#39)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -206,7 +206,6 @@ public class RobotContainer {
         )
     ).onFalse(
       new ParallelCommandGroup(
-        new InstantCommand(()->index.setMotorSpeed(0), index),
         new InstantCommand(()->shooter.shootFlywheelOnRPM(0), shooter)
     )); // Spin Shooter OUT
     
@@ -297,6 +296,11 @@ public class RobotContainer {
   }
 
   public void teleopPeriodic(){
+
+  }
+
+  public void robotPeriodic(){
+
     Pose2d visionPose = limelight.getPose();
 
     SmartDashboard.putNumber("LL X pose", visionPose.getX());
@@ -314,11 +318,9 @@ public class RobotContainer {
       if (dist <= 1000){
         double latencySec = limelight.getCaptureLatency() + limelight.getPipelineLatency();
         drivetrain.addVisionMeasurement(visionPose, latencySec/1000);
+
       }
     }
-  }
-
-  public void robotPeriodic(){
 
     SmartDashboard.putNumber("Current RPM", shooter.getFlywheelRPM());
     SmartDashboard.putNumber("Current Setpoint RPM", shooter.MANUAL_RPM);
