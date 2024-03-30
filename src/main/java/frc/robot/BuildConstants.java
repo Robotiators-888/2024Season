569061882177d157da375eeea2f0c0d224adfece
KevinH45 <timmyhwang127@gmail.com>
2024-03-30 10:16:12 -0400

    added fallback strategy

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2024Season";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 107;
-  public static final String GIT_SHA = "76404c99802dd569fba626647ea6c173b441bdb9";
-  public static final String GIT_DATE = "2024-03-30 09:33:11 EDT";
+  public static final int GIT_REVISION = 108;
+  public static final String GIT_SHA = "30e62cfc2fdf79e6f0a379243452bcbb8db4d53b";
+  public static final String GIT_DATE = "2024-03-30 10:14:18 EDT";
   public static final String GIT_BRANCH = "Week4-OwingsMills";
-  public static final String BUILD_DATE = "2024-03-30 10:12:07 EDT";
-  public static final long BUILD_UNIX_TIME = 1711807927664L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2024-03-30 10:14:47 EDT";
+  public static final long BUILD_UNIX_TIME = 1711808087184L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
