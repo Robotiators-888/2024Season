02e00c5fa122279df59e622ccb54a404436df3c7
Spencer McGrath <Samcgrath2005@gmail.com>
2024-04-01 11:02:56 -0400

    Stupid Atags

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2024Season";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 113;
-  public static final String GIT_SHA = "9c87268475e30b336478d2f73536fcc2ba780bce";
-  public static final String GIT_DATE = "2024-03-30 11:51:02 EDT";
+  public static final int GIT_REVISION = 114;
+  public static final String GIT_SHA = "ec7e6070a910ad59d881685d9509efca3e8ecb5d";
+  public static final String GIT_DATE = "2024-04-01 09:54:48 EDT";
   public static final String GIT_BRANCH = "Week4-OwingsMills";
-  public static final String BUILD_DATE = "2024-03-30 11:54:44 EDT";
-  public static final long BUILD_UNIX_TIME = 1711814084203L;
+  public static final String BUILD_DATE = "2024-04-01 10:10:18 EDT";
+  public static final long BUILD_UNIX_TIME = 1711980618436L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
