565939088bea306aca1058cd0c1a50ebbea7936b
KevinH45 <timmyhwang127@gmail.com>
2024-04-01 15:50:15 -0400

    hope fix aim on dist

--- src/main/java/frc/robot/commands/Limelight/CMD_TeleopAimOnDist.java
+++ src/main/java/frc/robot/commands/Limelight/CMD_TeleopAimOnDist.java
@@ -75,6 +75,8 @@ public class CMD_TeleopAimOnDist extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    currentPose = drivetrain.getPose();
+
     positionError = Math.sqrt(Math.pow(tagPose.getX() - currentPose.getX(), 2)
                            + Math.pow(tagPose.getY() - currentPose.getY(), 2));
 
@@ -88,13 +90,13 @@ public class CMD_TeleopAimOnDist extends Command {
     SmartDashboard.putNumber("X Error", xError);
     SmartDashboard.putNumber("Y Error", yError);
     SmartDashboard.putNumber("Angle", angle);
-    SmartDashboard.putNumber("Cur Rotation Radians", MathUtil.angleModulus(drivetrain.getRotation2d().getRadians()));
+    SmartDashboard.putNumber("Cur Rotation Radians", MathUtil.angleModulus(currentPose.getRotation().getRadians()));
     SmartDashboard.putNumber("Distance error", positionError);
 
     drivetrain.drive(
     -MathUtil.applyDeadband(Math.copySign(Math.pow(driverController.getRawAxis(1), 2), driverController.getRawAxis(1)), OIConstants.kDriveDeadband),
     -MathUtil.applyDeadband(Math.copySign(Math.pow(driverController.getRawAxis(0), 2), driverController.getRawAxis(0)), OIConstants.kDriveDeadband), 
-    robotAngleController.calculate(MathUtil.angleModulus(drivetrain.getRotation2d().getRadians()), angle),
+    robotAngleController.calculate(MathUtil.angleModulus(currentPose.getRotation().getRadians()), angle),
   true, true);
     
 
