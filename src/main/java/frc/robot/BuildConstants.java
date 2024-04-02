3319f88d3fa3a1393848942080ea83e9b220bfc9
KevinH45 <timmyhwang127@gmail.com>
2024-04-02 16:59:00 -0400

    fixed auto aim on dist

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2024Season";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 126;
-  public static final String GIT_SHA = "55dcd03a3bc20c7317587b0d10f96a841939f7c0";
-  public static final String GIT_DATE = "2024-04-02 08:59:27 EDT";
+  public static final int GIT_REVISION = 127;
+  public static final String GIT_SHA = "a16788f77e4f4b5addd668814d1c407002dcf2de";
+  public static final String GIT_DATE = "2024-04-02 15:10:56 EDT";
   public static final String GIT_BRANCH = "Week4-OwingsMills";
-  public static final String BUILD_DATE = "2024-04-02 15:07:57 EDT";
-  public static final long BUILD_UNIX_TIME = 1712084877522L;
+  public static final String BUILD_DATE = "2024-04-02 16:45:10 EDT";
+  public static final long BUILD_UNIX_TIME = 1712090710214L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
