badfc1487d314e8774416920b75d407fcd10dd8a
Spencer McGrath <Samcgrath2005@gmail.com>
2024-04-08 11:45:55 -0400

    changes

--- src/main/java/frc/robot/utils/AutoSelector.java
+++ src/main/java/frc/robot/utils/AutoSelector.java
@@ -23,28 +23,30 @@ public class AutoSelector {
     public AutoSelector(CommandXboxController driver1){
         autos = new AutoGenerator(driver1);
 
-        // chooser.setDefaultOption("1P_Stay", new AUTO_JustShoot());
-        // chooser.addOption("2P_Top", new AUTO_2P_Top());
-        // chooser.addOption("2P_Middle", new AUTO_2P_Mid());
+        chooser.setDefaultOption("1P_Stay", new AUTO_JustShoot());
+        chooser.addOption("2P_Top", new AUTO_2P_Top());
+        chooser.addOption("2P_Middle", new AUTO_2P_Mid());
 
-        // chooser.addOption("2P_Midline_Bottom_Two", new AUTO_MidLine_Bottom_Two());
+        chooser.addOption("2P_Midline_Bottom_Two", new AUTO_MidLine_Bottom_Two());
 
-        // chooser.addOption("3P_Top", new AUTO_3P_Top());
-        // chooser.addOption("3P_Middle", new AUTO_3P_Mid());
-        // chooser.addOption("3P_Midline_ONLY_Bottom", new AUTO_MidLine_Bottom_Three());
-        // chooser.addOption("3P_Midline_Top", new AUTO_3P_Playoffs());
-
-
-        // chooser.addOption("4P_MidLine_3_1st", new AUTO_MidLine_FourP_One());
-        //chooser.addOption("4P_Autoshot", new AUTO_MidLine_FourP_One_AIM());
-
-        // chooser.addOption("4P_Top", new AUTO_4P_Top());
-        // chooser.addOption("4P_Middle", new AUTO_4P_Mid());
-        chooser.setDefaultOption("4P_Middle PLAYOFFS!!!!!", new AUTO_4P_Mid_PLAYOFFS());
+        chooser.addOption("3P_Top", new AUTO_3P_Top());
+        chooser.addOption("3P_Middle", new AUTO_3P_Mid());
+        chooser.addOption("3P_Midline_ONLY_Bottom", new AUTO_MidLine_Bottom_Three());
+        chooser.addOption("3P_Midline_Top", new AUTO_3P_Playoffs());
 
 
-        // chooser.addOption("5P_Middle_No_Stage", new AUTO_5P_Mid_NO_STAGE());
-        // chooser.addOption("5P_Middle_STAGE", new AUTO_5P_Mid_STAGE());
+        chooser.addOption("4P_MidLine_3_1st", new AUTO_MidLine_FourP_One());
+        chooser.addOption("4P_Autoshot", new AUTO_MidLine_FourP_One_AIM());
+
+        chooser.addOption("4P_Top", new AUTO_4P_Top());
+        chooser.addOption("4P_Middle", new AUTO_4P_Mid());
+
+        // DCMP AUTO
+        // chooser.addOption("4P_Middle PLAYOFFS!!!!!", new AUTO_4P_Mid_PLAYOFFS());
+
+
+        chooser.addOption("5P_Middle_No_Stage", new AUTO_5P_Mid_NO_STAGE());
+        chooser.addOption("5P_Middle_STAGE", new AUTO_5P_Mid_STAGE());
 
         // chooser.addOption("Gerstner Auto", new AUTO_Silly_GERSTNER());
 
